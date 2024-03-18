216a4aa3474a81f85cdb405a20ab600dd8b87e40
Blblblbl42 <khaldi.sofiain@gmail.com>
2024-03-18 21:44:20 +0100

    Modification de la table

--- sanstitre0.py
+++ sanstitre0.py
@@ -23,18 +23,29 @@ def upload_form():
 
         # On prend le curseur et on lui fait exécuter une commande SQL pour créer une table si elle n'existe pas
         cur.execute("""
-            CREATE TABLE IF NOT EXISTS Tableau (
+            CREATE TABLE Tableau (
                 id SERIAL PRIMARY KEY,
                 nom VARCHAR(255),
                 type VARCHAR(255),
                 craft_1 VARCHAR(255),
+                nombre_1 INT,
                 craft_2 VARCHAR(255),
+                nombre_2 INT,
                 craft_3 VARCHAR(255),
+                nombre_3 INT,
                 craft_4 VARCHAR(255),
+                nombre_4 INT,
                 craft_5 VARCHAR(255),
+                nombre_5 INT,
                 craft_6 VARCHAR(255),
+                nombre_6 INT,
                 craft_7 VARCHAR(255),
-                craft_8 VARCHAR(255)
+                nombre_7 INT,
+                craft_8 VARCHAR(255),
+                nombre_8 INT,
+                prix_craft FLOAT,
+                prix_item FLOAT,
+                Bénéfice FLOAT
             )
         """)
         # On valide les modifications
